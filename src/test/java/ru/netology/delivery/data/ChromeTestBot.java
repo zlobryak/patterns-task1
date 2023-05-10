9ac1dbb84482ab45e532fbd7085c4843875be9e4
Neto Homework <clychnik@gmail.com>
2023-05-10 22:40:01 +0300

    Javadoc for ChromeTestBot.java

--- src/test/java/ru/netology/delivery/data/ChromeTestBot.java
+++ src/test/java/ru/netology/delivery/data/ChromeTestBot.java
@@ -3,6 +3,16 @@ package ru.netology.delivery.data;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ChromeTestBot {
+    /**
+     * @param shiftDateToSet int to shift date
+     * @param dateFormat dd.MM.yyyy for app-card-delivery.jar
+     * @param city Use "random" or test data
+     * @param name Use "random" or test data
+     * @param phone Use "random" or test data
+     * @param agreement True to check the box and false to not check
+     * @param buttonText Use text for search the button from all the buttons at page
+     * @return Returns generated date.
+     */
     public static String autoRegistration(
             int shiftDateToSet,
             String dateFormat,
